d5b8ce2f18a879ed698a76561c63257f74cf1c68
Toma955 <78325663+Toma955@users.noreply.github.com>
2025-06-16 17:20:28 +0200

    Added icon indicators with smooth transitions between icons

--- anamarijaapp/src/StartScreen.js
+++ anamarijaapp/src/StartScreen.js
@@ -41,6 +41,10 @@ const StartScreen = ({ onStart }) => {
     if (buttonContainer) {
       buttonContainer.classList.add('clicked');
     }
+    const iconsContainer = document.querySelector('.icons-container');
+    if (iconsContainer) {
+      iconsContainer.classList.add('show-headset-indicator');
+    }
     setTimeout(() => {
       const progressContainer = document.querySelector('.progress-container');
       if (progressContainer) {
@@ -126,7 +130,18 @@ const StartScreen = ({ onStart }) => {
     if (currentMessage === 0) {
       setCurrentMessage(1);
       setDisplayedText(renderMessageContent(1));
+      // Move circle to info icon
+      const iconsContainer = document.querySelector('.icons-container');
+      if (iconsContainer) {
+        iconsContainer.classList.add('show-info-indicator');
+      }
     } else if (currentMessage === 1) {
+      // Move circle to question mark icon
+      const iconsContainer = document.querySelector('.icons-container');
+      if (iconsContainer) {
+        iconsContainer.classList.add('show-question-indicator');
+      }
+      
       setIsTransitioning(true);
       
       const welcomeText = document.querySelector('.welcome-text');
@@ -241,6 +256,20 @@ const StartScreen = ({ onStart }) => {
 
   return (
     <div className="start-screen">
+      <div className="icons-container">
+        <div className="icon-item">
+          <i className="fa-solid fa-play"></i>
+        </div>
+        <div className="icon-item">
+          <i className="fa-solid fa-headphones"></i>
+        </div>
+        <div className="icon-item">
+          <i className="fa-solid fa-circle-info"></i>
+        </div>
+        <div className="icon-item">
+          <i className="fa-solid fa-circle-question"></i>
+        </div>
+      </div>
       <div className="landscape-warning">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white">
           <path d="M16.48 2.52c3.27 1.55 5.61 4.72 5.97 8.48h1.5C23.44 4.84 18.29 0 12 0l-.66.03 3.81 3.81 1.33-1.32zm-6.25-.77c-.59-.59-1.54-.59-2.12 0L1.75 8.11c-.59.59-.59 1.54 0 2.12l12.02 12.02c.59.59 1.54.59 2.12 0l6.36-6.36c.59-.59.59-1.54 0-2.12L10.23 1.75zm4.6 19.44L2.81 9.17l6.36-6.36 12.02 12.02-6.36 6.36zm-7.31.29C4.25 19.94 1.91 16.76 1.55 13H.05C.56 19.16 5.71 24 12 24l.66-.03-3.81-3.81-1.33 1.32z"/>
